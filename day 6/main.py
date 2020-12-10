9468b6d7d47705d441d44ccdb92408e2167dd2d3
Austinae <austin.william.ga@gmail.com>
2020-12-10 15:22:47 +0100

    day 6

--- day 6/main.py
+++ day 6/main.py
@@ -2,10 +2,10 @@ with open("input.txt") as f:
     inp = f.readlines()
 inp = [x.strip() for x in inp]
 
-grpans = []
-res = 0
-
-#part1
+# part1
+# grpans = []
+# res = 0
+#
 # def getuniquefrommultiset(grpans):
 #     unitedgrpans = ""
 #     uniqueset = ""
@@ -27,4 +27,27 @@ res = 0
 #         res += getuniquefrommultiset(grpans)
 #         grpans = []
 
+# part2
+# grpans = []
+# res = 0
+#
+# def getcommonvalue(grpans):
+#     j = []
+#     for e in grpans:
+#         g = []
+#         for k in e:
+#             g.append(k)
+#         j.append(g)
+#     return len(list(set.intersection(*map(set, j))))
+#
+#
+# for x, i in enumerate(inp):
+#     if i:
+#         grpans.append(i)
+#         if x == len(inp)-1:
+#             res += getcommonvalue(grpans)
+#         continue
+#     if not(i):
+#         res += getcommonvalue(grpans)
+#         grpans = []
 
